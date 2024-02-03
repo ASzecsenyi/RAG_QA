ed98b9f45b3a75158320dc9f65cc004afd5858d4
Andras Szecsenyi <andras.szecsenyi@recourseai.com>
2024-02-03 11:03:09 +0000

    Add SentChunker.py

---
+++ retrieval/Chunker/SentChunker.py
@@ -0,0 +1,32 @@
+import nltk
+
+from retrieval.Chunker import Chunker
+
+# if needed, download the punkt tokenizer
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+
+class SentChunker(Chunker):
+    def __init__(self, chunk_length: int, sliding_window_size: float = 0.0):
+        """
+        :param chunk_length: The length of the chunks, measured in sentences.
+        :type chunk_length: int
+        :param sliding_window_size: The size of the sliding window. Defines the overlap between chunks, rounded down. Must be between 0.0 and 1.0, defaults to 0.0.
+        :type sliding_window_size: float, optional
+        """
+        super().__init__(chunk_length, sliding_window_size)
+
+    def chunk(self, document: str) -> list[str]:
+        sentences = nltk.sent_tokenize(document)
+
+        return [' '.join(sentences[i:i + self.chunk_length]) for i in
+                range(
+                    0,
+                    len(sentences),
+                    self.chunk_length - int(self.chunk_length * self.sliding_window_size)
+                )]
+
+
--- tests/test_chunker.py
+++ tests/test_chunker.py
@@ -1,6 +1,7 @@
 import pytest
 from retrieval.Chunker import Chunker
 from retrieval.Chunker.CharChunker import CharChunker
+from retrieval.Chunker.SentChunker import SentChunker
 
 
 def test_chunker_init():
@@ -30,3 +31,24 @@ def test_char_chunker_chunk():
     c = CharChunker(3, 0.0)
     chunks = c.chunk(document)
     assert chunks == ['abc', 'def', 'gh']
+
+
+def test_sent_chunker_init():
+    c = SentChunker(10, 0.5)
+    assert c.chunk_length == 10
+    assert c.sliding_window_size == 0.5
+    assert c.name == 'SentChunker_10_0.5'
+
+
+def test_sent_chunker_chunk():
+    document = 'Aa. Bb. Cc. dd. Ee. ff. Gg. Hh.'
+
+    c = SentChunker(3, 0.0)
+    chunks = c.chunk(document)
+    assert chunks == ['Aa. Bb. Cc.', 'dd. Ee. ff.', 'Gg. Hh.'], f"window size 0.0"
+
+    c = SentChunker(3, 0.5)
+    chunks = c.chunk(document)
+    assert chunks == ['Aa. Bb. Cc.', 'Cc. dd. Ee.', 'Ee. ff. Gg.', 'Gg. Hh.'], f"window size 0.5"
+
+
