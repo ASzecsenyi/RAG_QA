5a6609c1860af226af151adc424b7d4d64e3124f
Andras Szecsenyi <andras.szecsenyi@recourseai.com>
2024-02-04 19:52:36 +0000

    Update Admin page

--- experiment_app/admin.py
+++ experiment_app/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Experiment, ExperimentChunker, ExperimentQA, ExperimentRanker, ExperimentTextDocument
+
+
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
+    search_fields = ['name']
+    list_filter = []
+
+
+@admin.register(ExperimentTextDocument)
+class ExperimentTextDocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'experiment', 'question', 'file', 'file_path']
+    search_fields = ['experiment__name']
+    list_filter = ['experiment']
+
+
+@admin.register(ExperimentChunker)
+class ExperimentChunkerAdmin(admin.ModelAdmin):
+    list_display = ['chunker_name', 'id', 'experiment', 'chunk_length', 'sliding_window_size']
+    search_fields = ['chunker_name', 'experiment__name']
+    list_filter = ['experiment']
+
+
+@admin.register(ExperimentRanker)
+class ExperimentRankerAdmin(admin.ModelAdmin):
+    list_display = ['ranker_name', 'id', 'experiment', 'top_k']
+    search_fields = ['ranker_name', 'experiment__name']
+    list_filter = ['experiment']
+
+
+@admin.register(ExperimentQA)
+class ExperimentQAAdmin(admin.ModelAdmin):
+    list_display = ['model_name', 'id', 'experiment']
+    search_fields = ['model_name', 'experiment__name']
+    list_filter = ['experiment']
