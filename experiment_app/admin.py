00614c79ffee17a3035f5474fa6bc87190b09a0d
Andras Szecsenyi <andras.szecsenyi@recourseai.com>
2024-02-06 19:44:29 +0000

    Update Admin page for new models

--- experiment_app/admin.py
+++ experiment_app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Experiment, ExperimentChunker, ExperimentQA, ExperimentRanker, ExperimentTextDocument
+from .models import Experiment, ExperimentChunker, ExperimentQA, ExperimentRanker, ExperimentTextDocument, \
+    ExperimentNewsQaDocument
 
 
 @admin.register(Experiment)
@@ -19,22 +20,29 @@ class ExperimentTextDocumentAdmin(admin.ModelAdmin):
     list_filter = ['experiment']
 
 
+@admin.register(ExperimentNewsQaDocument)
+class ExperimentNewsQaDocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'experiment', 'story_id']
+    search_fields = ['experiment__name']
+    list_filter = ['experiment']
+
+
 @admin.register(ExperimentChunker)
 class ExperimentChunkerAdmin(admin.ModelAdmin):
-    list_display = ['chunker_name', 'id', 'experiment', 'chunk_length', 'sliding_window_size']
+    list_display = ['chunker_type', 'chunker_name', 'id', 'experiment', 'chunk_length', 'sliding_window_size']
     search_fields = ['chunker_name', 'experiment__name']
     list_filter = ['experiment']
 
 
 @admin.register(ExperimentRanker)
 class ExperimentRankerAdmin(admin.ModelAdmin):
-    list_display = ['ranker_name', 'id', 'experiment', 'top_k']
+    list_display = ['ranker_type', 'ranker_name', 'id', 'experiment', 'top_k']
     search_fields = ['ranker_name', 'experiment__name']
     list_filter = ['experiment']
 
 
 @admin.register(ExperimentQA)
 class ExperimentQAAdmin(admin.ModelAdmin):
-    list_display = ['model_name', 'id', 'experiment']
+    list_display = ['model_type', 'model_name', 'id', 'experiment']
     search_fields = ['model_name', 'experiment__name']
     list_filter = ['experiment']
