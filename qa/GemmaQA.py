86c626a079e2b43899275707d5861993a57201c8
Andras Szecsenyi <andras.szecsenyi@recourseai.com>
2024-02-26 20:58:46 +0000

    Add components

---
+++ qa/GemmaQA.py
@@ -0,0 +1,83 @@
+import os
+import time
+
+import requests
+
+from qa import QA
+
+
+class GemmaQA(QA):
+    """
+    Mistral QA model.
+    """
+
+    default_prompt = (
+        "You are a question answering agent."
+        "Generate your response by following the steps below: "
+        "1. Read the context and the question. "
+        "2. Select the most relevant information from the context. "
+        "3. Determine whether the question can be answered based on the context. "
+        "4. If the question can be answered, generate a draft answer. "
+        "5. Validate that the draft answer is completely grounded in the context. "
+        "6. Generate your final answer and include the exact text from the context that supports your answer. Generate a single-sentence response that is clear, concise, and helpful. "
+        "7. If the question cannot be answered with the context, say [UNKNOWN]. This will be your final answer in this case. "
+        "8. Only show your final answer! Do not provide any explanation or reasoning. "
+        ""
+        "CONTEXT: {chunks} "
+        ""
+        "QUESTION: {question}")
+
+    def __init__(self, name: str, prompt: str = default_prompt, api_key: str = None):
+        super().__init__(name)
+        self.api_key = api_key
+        self.prompt = prompt
+        if api_key is None:
+            try:
+                self.api_key = os.environ["HUGGINGFACE_API_KEY"]
+            except KeyError:
+                raise KeyError(
+                    "HUGGINGFACE_API_KEY environment variable not found. Please set it or pass it as an argument."
+                )
+        if name is None:
+            self.name += f"_{self.api_key[-5:]}"
+
+    def predict(self, question: str, chunks: list[str]) -> str:
+        """
+        Predicts the answer to a question given a context.
+
+        :param question: the question
+        :type question: str
+        :param chunks: the context
+        :type chunks: list[str]
+
+        :return: the answer
+        :rtype: str
+        """
+
+        inputs = self.prompt.format(chunks=" ".join(chunks), question=question)
+
+        time.sleep(0.1)
+        try:
+            response = requests.post(
+                "https://api-inference.huggingface.co/models/google/gemma-7b-it",
+                headers={"Authorization": f"Bearer {self.api_key}"},
+                json={
+                    "inputs": inputs,
+                    "parameters": {
+                        "max_new_tokens": 100,
+                    }
+                }
+            )
+        except requests.exceptions.ConnectionError:
+            print("Paused due to lost connection. Press enter to continue.")
+            return self.predict(question, chunks)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            # if too many requests are made, the server will return a 503 status code
+            print("Too many requests. Waiting 10 seconds.")
+            time.sleep(10)
+            return self.predict(question, chunks)
+
+        return response.json()[0]["generated_text"].split("[/INST]")[-1].strip()
---
+++ retrieval/Chunker/WordChunker.py
@@ -0,0 +1,32 @@
+import nltk
+
+from retrieval.Chunker import Chunker
+
+# if needed, download the punkt tokenizer
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+
+class WordChunker(Chunker):
+    def __init__(self, chunk_length: int, sliding_window_size: float = 0.0, name: str = None):
+        """
+        :param chunk_length: The length of the chunks, measured in sentences.
+        :type chunk_length: int
+        :param sliding_window_size: The size of the sliding window. Defines the overlap between chunks, rounded down. Must be between 0.0 and 1.0, defaults to 0.0.
+        :type sliding_window_size: float, optional
+        """
+        super().__init__(chunk_length, sliding_window_size, name)
+
+    def chunk(self, document: str) -> list[str]:
+        words = nltk.word_tokenize(document)
+
+        return [' '.join(words[i:i + self.chunk_length]) for i in
+                range(
+                    0,
+                    len(words),
+                    self.chunk_length - int(self.chunk_length * self.sliding_window_size)
+                )]
+
+
